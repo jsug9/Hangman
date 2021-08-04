7cf40f714fb2ae20b8ffb6849495ca925f09ea1b
jsug9 <75434179+jsug9@users.noreply.github.com>
2021-08-04 10:32:14 -0500

    Initial Commit

---
+++ Apple Pie SUI/Apple_Pie_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Pie_SUIApp.swift
+//  Apple Pie SUI
+//
+//  Created by Augusto Galindo Alí on 4/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Pie_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Pie SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple Pie SUI
+//
+//  Created by Augusto Galindo Alí on 4/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
